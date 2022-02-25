9ba85dcd3c594b5e3c8a9fc9805cc5436b4b5e4e
Maggie9685 <maggielai9685@gmail.com>
2022-02-24 20:27:41 -0800

    build header, navbar and page for according section

---
+++ src/components/NavTabs.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs justify-content-end">
+      <div className="container-fluid fs-1 ">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Maggie Lai
+        </a>
+      </div>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#porfolio"
+          onClick={() => handlePageChange('Porfolio')}
+          className={currentPage === 'Porfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Porfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
